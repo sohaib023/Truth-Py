51a30cc1e8a7490001eb49e01979d868b91f63c0
sohaib023 <sohaib023@gmail.com>
2020-12-28 17:11:34 +0500

    convert pytruth to a PyPi package named truthpy

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='truthpy',
+   version='1.0',
+   description='Module for handling labelled XML files from T-Truth',
+   author='Sohaib Zahid',
+   author_email='sohaib023@gmail.com',
+   packages=['truthpy'],  #same as name
+)
---
+++ truthpy/__init__.py
@@ -0,0 +1,4 @@
+from .Document import Document
+from .Table import Table
+from .Rect import Rect
+from .GTElement import GTElement, Row, Column, RowSpan, ColSpan, Cell
